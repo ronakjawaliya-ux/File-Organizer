5884cf4f1635476b9176ab63613708671cc76560
Ronak Jawalia <ronakjawaliya@gmail.com>
2026-08-08 01:23:58 +0530

    Add PDF file organization

--- main.py
+++ main.py
@@ -3,22 +3,20 @@ import shutil
 
 
 def main():
-    pass
 
-folder_path = "sample_files"
+    folder_path = "sample_files"
+    files = os.listdir(folder_path)
+    print(files)
 
-files = os.listdir(folder_path)
+    os.makedirs(os.path.join(folder_path, "Resume"), exist_ok=True)
 
-files = [
-    "resume.pdf",
-    "photo.jpg",
-    "notes.txt",
-    "book.pdf"
-]
+    for file in files:
+        if file.endswith(".pdf"):
+            source_file = os.path.join(folder_path, file)
+            destination_file = os.path.join(folder_path,"Resume", file)
+            shutil.move(source_file,destination_file)
 
-for file in files:
-    if file.endswith(".pdf"):
-        print(file)
+
 
 if __name__ == "__main__":
     main()
