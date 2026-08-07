bf3c8832cc8cf5f22755d18572b3bf10634cf628
Ronak Jawalia <ronakjawaliya@gmail.com>
2026-08-07 23:30:04 +0530

    Improvements

--- main.py
+++ main.py
@@ -5,6 +5,20 @@ import shutil
 def main():
     pass
 
+folder_path = "sample_files"
+
+files = os.listdir(folder_path)
+
+files = [
+    "resume.pdf",
+    "photo.jpg",
+    "notes.txt",
+    "book.pdf"
+]
+
+for file in files:
+    if file.endswith(".pdf"):
+        print(file)
 
 if __name__ == "__main__":
     main()
